96c9c7da4ea07470451837fc078b0cc650cccd6d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-11 12:42:51 -0400

    Installs 'supertest' as a devDependency and imports it into server.spec.js as 'request'

---
+++ api/server.spec.js
@@ -0,0 +1,9 @@
+// testing endpoints
+// returns correct http status code
+describe('server.js', () => {
+    describe('GET /', () => {
+        it('should respond with 200 OK', () => {
+            
+        });
+    });
+});
