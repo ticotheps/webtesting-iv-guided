972135d56f80fc476f7ea1895315f86325124930
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-11 12:44:27 -0400

    Imports 'server.js' into the server.spec.js file

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,9 +1,12 @@
+const request = require('supertest');
+const server = require('./server.js');
+
 // testing endpoints
 // returns correct http status code
 describe('server.js', () => {
     describe('GET /', () => {
         it('should respond with 200 OK', () => {
-            
+
         });
     });
 });
